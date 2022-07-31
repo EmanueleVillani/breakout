a2c76d049f9dd2204ddb6bf3095ef4a86ecb8bee
Emanuele Villani <manu.lebon@hotmail.it>
2022-07-31 18:01:18 +0200

    fix
    fix

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-#if UNITY_EDITOR
 using UnityEditor;
 using TMPro;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+
+
 #endif
+
+
 
 public class UIManager : MonoBehaviour
 {
@@ -17,8 +21,8 @@ public class UIManager : MonoBehaviour
 
     public Slider difSlider;
     public TextMeshProUGUI difficultyText;
+   
 
-    // Start is called before the first frame update
     void Start()
     {
         HighscoreText.text = "Best Score : " + MenuManager.Instance.names[0] + " : " + MenuManager.Instance.points[0];
@@ -31,9 +35,14 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void BtnClassific()
+    {
+        SceneManager.LoadScene(2);
+    }
+
     public void Exit()
     {
-        // GameManager.Instance.SaveName();
+      
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
@@ -57,9 +66,6 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void BtnClassific()
-    {
-        SceneManager.LoadScene(2);
-    }
+    
 }
 
